d3f52b06285f80275324aba0579af5fc416d4e07
Avery Yang <kcih4518@gmail.com>
2023-04-17 17:01:25 +0800

    feat(util): Create RandomUtil to generate test data
    Create a RandomUtil module to generate randomized test data for use in unit tests.
    The module will provide functions for generating random strings, integers, and other common data types, in order to create diverse test cases.

---
+++ util/random.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	// Seed the random number generator with the current time.
+	// If not seeded, the generator will produce the same sequence of numbers
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt returns a random number in the range [min, max]
+func RandomInt(min, max int64) int64 {
+	// rand.Int63n(max-min+1) returns a random number in the range [0, max-min+1)
+	// min + rand.Int63n(max-min+1) returns a random number in the range [min, max]
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString returns a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		sb.WriteByte(alphabet[rand.Intn(k)])
+	}
+	return sb.String()
+}
+
+// RandomOwner returns a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney returns a random money amount
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency returns a random currency
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
