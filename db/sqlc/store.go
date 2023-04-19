99e878ad8b3dc2e2873c2cc73e08e35f4eef539e
Avery Yang <kcih4518@gmail.com>
2023-04-19 16:50:15 +0800

    feat(db): Implement database transactions
    Implement database transactions to maintain data integrity when executing complex database operations.
    Using database transactions ensures that commits are made when operations succeed, and rollbacks are performed when operations fail, preventing data inconsistencies.

---
+++ db/sqlc/store.go
@@ -0,0 +1,102 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// store provides all functions to execute database queries and transactions.
+// It also provides a way to create a new store with a transaction.
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new store.
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes a function within a database transaction.
+// don't export this function, it's only used internally
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx) // create a new store with the transaction
+	err = fn(q)  // execute the function with the new store
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	// commit the transaction
+	return tx.Commit()
+}
+
+// TransferTxParams contains the input parameters for the TransferTx function.
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// TransferTxResult contains the result of the TransferTx function.
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TransferTx performs a money transfer from one account to another.
+// It creates a transfer record and updates account balances within a database transaction.
+// Why use closure? Because we want to use the same transaction for all queries.
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		// TODO: Update account's balance
+
+		return nil
+	})
+	return result, err
+}
