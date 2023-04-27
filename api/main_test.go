bc1f7cb109edb74d5820a27ec72b103d2115d1c4
Avery Yang <kcih4518@gmail.com>
2023-04-27 23:58:27 +0800

    test(gin): Set up Gin to test mode during go test command

---
+++ api/main_test.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+// TestMain is the entry point for all tests
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
+}
