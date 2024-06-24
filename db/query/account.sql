ecbc4194e0f71806e0cb86f460d8a84ecefdab91
Avery Yang(楊長珈) <avery_yang@asus.com>
2024-06-24 20:22:10 +0800

    feat: add CreateAccount functionality with SQL query and Go method

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,3 +1,16 @@
+-- name: CreateAccount :one
+INSERT INTO accounts
+  (
+  owner,
+  balance,
+  currency
+  )
+VALUES
+  (
+    $1, $2, $3
+)
+RETURNING *;
+
 -- name: GetAccount :one
 SELECT *
 FROM accounts
